3737e1cbafa37d7f27c08943249022e11592e471
Frederico Martini <fredmalmeida@gmail.com>
2025-09-25 08:17:40 -0300

    feat: CV-11811 - Add new Type for CreateConversationMessageFromType (MessageID)

--- src/enums/CreateConversationFromType.ts
+++ src/enums/CreateConversationFromType.ts
@@ -1,5 +1,12 @@
 export enum CreateConversationMessageFromType {
+  /**
+   * @deprecated Use MessageId instead, will be removed in the future (2026-01-01).
+   */
   PreRecorded = 'PreRecorded',
   NewMessage = 'NewMessage',
   Forward = 'Forward',
+  /**
+   * @description MessageId will be used for PreRecorded and VoiceMemo.
+   */
+  MessageId = 'MessageId',
 }
